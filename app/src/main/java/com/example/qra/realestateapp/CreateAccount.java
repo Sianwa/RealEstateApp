405d3c105b30c3dd1eb17c259f72c11e1cd59b32
Sinwa <sianwamak@gmail.com>
2019-03-11 10:11:14 +0300

    User sign up and login are active

--- app/src/main/java/com/example/qra/realestateapp/CreateAccount.java
+++ app/src/main/java/com/example/qra/realestateapp/CreateAccount.java
@@ -1,12 +1,24 @@
 package com.example.qra.realestateapp;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity {
     EditText _txtusername,_txtemail,_txtpass;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +31,37 @@ public class CreateAccount extends AppCompatActivity {
         _txtemail = (EditText) findViewById(R.id.txtemail);
         _txtpass = (EditText) findViewById(R.id.txtpass);
         Button _btnreg = (Button) findViewById(R.id.btn_reg);
+        _btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
+
+        mAuth=FirebaseAuth.getInstance();
+        //check to see if the user is currently signed in
     }
-}
+
+    private void createAccount() {
+        String email=_txtemail.getText().toString();
+        String password=_txtpass.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    //checks status of task proceeding it
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(CreateAccount.this, "Signed in",Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(CreateAccount.this, MainMenu.class);
+                            startActivity(intent);
+                        }
+
+
+                    }
+                });
+        }
+    }
+
--- app/src/main/java/com/example/qra/realestateapp/LoginPage.java
+++ app/src/main/java/com/example/qra/realestateapp/LoginPage.java
@@ -1,12 +1,19 @@
 package com.example.qra.realestateapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
     EditText _txtemail;
@@ -14,29 +21,65 @@ public class LoginPage extends AppCompatActivity {
     Button _btnlogin;
     TextView _btnreg;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
+        mAuth=FirebaseAuth.getInstance();
+
         //referencing username,password editText and textview for singup
 
         _btnlogin =(Button)findViewById(R.id.buttonLogin);
-        //  _btnlogin.setOnClickListener(this);
-        _btnreg=(TextView)findViewById(R.id.btnreg) ;
+        _btnreg=(TextView)findViewById(R.id.btnreg);
 
         _txtemail=(EditText) findViewById(R.id.txtemail);
         _txtpass=(EditText) findViewById(R.id.txtpass);
 
 
-        _btnreg.setOnClickListener(new View.OnClickListener() {
+        _btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(LoginPage.this, CreateAccount.class);
-                intent.putExtra("EMAIL",_txtemail.getText().toString().trim());
-                startActivity(intent);
+            public void onClick(View v)
+            {
+             login();
             }
         });
+
+                _btnreg.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(LoginPage.this, CreateAccount.class);
+                        intent.putExtra("EMAIL", _txtemail.getText().toString().trim());
+                        startActivity(intent);
+                    }
+                });
+
+    }
+
+    private void login() {
+        String email=_txtemail.getText().toString();
+        String password =_txtpass.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginPage.this, "Signed in",Toast.LENGTH_LONG).show();
+
+                        Intent intent =new Intent(LoginPage.this, MainMenu.class);
+                        startActivity(intent);
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginPage.this,"Wrong Credentials",Toast.LENGTH_LONG).show();
+                        }
+
+
+                    }
+                });
 
     }
 }
